7b9f199aa122870f15a7315b8020fccf03036fe6
Raúl Martín <55482385+rmartinsanta@users.noreply.github.com>
2020-05-08 13:18:34 +0200

    Comment ExchangerILS config

--- src/main/java/es/urjc/etsii/grafo/solver/algorithms/config/ExchangerILSConfig.java
+++ src/main/java/es/urjc/etsii/grafo/solver/algorithms/config/ExchangerILSConfig.java
@@ -1,27 +1,27 @@
-package es.urjc.etsii.grafo.solver.algorithms.config;
-
-import es.urjc.etsii.grafo.solver.algorithms.ExchangerILS;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ConfigurationProperties(prefix = "algorithms.exchangerils")
-public class ExchangerILSConfig implements AlgorithmConfig{
-    private int nRotateRounds;
-    private ExchangerILS.ILSConfig[] configs;
-
-    public ExchangerILSConfig(int nRotateRounds, ExchangerILS.ILSConfig[] configs) {
-        this.nRotateRounds = nRotateRounds;
-        this.configs = configs;
-    }
-
-    protected ExchangerILSConfig() { }
-
-    public int getnRotateRounds() {
-        return nRotateRounds;
-    }
-
-    public ExchangerILS.ILSConfig[] getConfigs() {
-        return configs;
-    }
-}
+//package es.urjc.etsii.grafo.solver.algorithms.config;
+//
+//import es.urjc.etsii.grafo.solver.algorithms.ExchangerILS;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//@ConfigurationProperties(prefix = "algorithms.exchangerils")
+//public class ExchangerILSConfig implements AlgorithmConfig{
+//    private int nRotateRounds;
+//    private ExchangerILS.ILSConfig[] configs;
+//
+//    public ExchangerILSConfig(int nRotateRounds, ExchangerILS.ILSConfig[] configs) {
+//        this.nRotateRounds = nRotateRounds;
+//        this.configs = configs;
+//    }
+//
+//    protected ExchangerILSConfig() { }
+//
+//    public int getnRotateRounds() {
+//        return nRotateRounds;
+//    }
+//
+//    public ExchangerILS.ILSConfig[] getConfigs() {
+//        return configs;
+//    }
+//}
