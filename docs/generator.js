b67dec5a9aa00818b757040664af23eb68698e82
Raúl Martín <raul.martin@urjc.es>
2021-05-11 12:15:53 +0200

    Fix bug in path in generator

--- docs/generator.js
+++ docs/generator.js
@@ -38,10 +38,10 @@ function generateProject(){
 
   urls.forEach(function(url){
     // loading a file and add it in a zip file
-    var fullURL = domain + url.folder + '/' + url.name;
-    var fullURL = fullURL.replaceAll('//', '/');
+    var path = domain + url.folder + '/' + url.name;
+    var path = path.replaceAll('//', '/');
 
-    JSZipUtils.getBinaryContent(fullURL, function (err, data) {
+    JSZipUtils.getBinaryContent(domain + path, function (err, data) {
       log("Downloading " + fullURL);
        if(err) {
          log("Error found while downloading " + fullURL);
