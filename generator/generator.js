23744407f827be91ff2040bd31e49e03ea4fa41a
Raúl Martín <raul.martin@urjc.es>
2021-05-11 02:17:51 +0200

    Finished generator website

--- generator/generator.js
+++ generator/generator.js
@@ -1,29 +1,68 @@
-var zip = new JSZip();
-var count = 0;
-var mark = '%%_NAME_%%';
-var correctName = 'KProp';
+function log(message){
+  var textarea = document.getElementById('logs');
+  textarea.value += message;
+  textarea.value += '\n';
+  textarea.scrollTop = textarea.scrollHeight;
+}
 
-var domain = 'https://github.com/rmartinsanta/mork/tree/web/template';
-var urls = [
-  'asdfg.java',
-  'asdasd/asdasd/asd.java',
-  'asdasdasd.java'
-];
+function generateProject(){
 
+  var inputElement = document.getElementById('projectname')
+  if(inputElement.validity.patternMismatch){
+    log("ERROR: Invalid project name. Check that it starts with an Uppercase letter followed by any alphanumeric characters or underscores");
+    return;
+  }
+  var correctName = inputElement.value;
 
-urls.forEach(function(url){
-  var filename = url;
-  // loading a file and add it in a zip file
-  JSZipUtils.getBinaryContent(domain + url, function (err, data) {
-     if(err) {
-        throw err; // or handle the error
-     }
-     zip.file(filename.replace(mark, correctName), data.replace(mark, correctName), {binary:true});
-     count++;
-     if (count == urls.length) {
-       zip.generateAsync({type:'blob'}).then(function(content) {
-          saveAs(content, correctName + ".zip");
-       });
-    }
+  log("Starting generation for project " + correctName);
+
+  // CONFIGURATION
+  var domain = 'https://raw.githubusercontent.com/rmartinsanta/mork/web/template/';
+  var urls = [
+    {folder: '', name: 'pom.xml'},
+    {folder: 'src/main/resources', name: 'application.yml'},
+    {folder: 'src/main/java/es/urjc/etsii/grafo/__RNAME__', name: 'Main.java'},
+    {folder: 'src/main/java/es/urjc/etsii/grafo/__RNAME__/constructives', name: '__RNAME__RandomConstructive.java'},
+    {folder: 'src/main/java/es/urjc/etsii/grafo/__RNAME__/constructives/grasp', name: '__RNAME__ListManager.java'},
+    {folder: 'src/main/java/es/urjc/etsii/grafo/__RNAME__/experiments', name: 'ConstructiveExperiment.java'},
+    {folder: 'src/main/java/es/urjc/etsii/grafo/__RNAME__/model', name: '__RNAME__Instance.java'},
+    {folder: 'src/main/java/es/urjc/etsii/grafo/__RNAME__/model', name: '__RNAME__InstanceImporter.java'},
+    {folder: 'src/main/java/es/urjc/etsii/grafo/__RNAME__/model', name: '__RNAME__Solution.java'}
+  ];
+
+  var zip = new JSZip();
+  var count = 0;
+  var mark = '__RNAME__';
+  var decoder = new TextDecoder("utf-8");
+  var encoder = new TextEncoder(); // Defaults to UTF-8
+
+  urls.forEach(function(url){
+    // loading a file and add it in a zip file
+    var fullURL = domain + url.folder + '/' + url.name;
+    var fullURL = fullURL.replaceAll('//', '/');
+
+    JSZipUtils.getBinaryContent(fullURL, function (err, data) {
+      log("Downloading " + fullURL);
+       if(err) {
+         log("Error found while downloading " + fullURL);
+         log(err);
+         throw err;
+       }
+       var realFolder = url.folder.replaceAll(mark, correctName);
+       var realFilename = url.name.replaceAll(mark, correctName);
+       var decodedContent = decoder.decode(data);
+       var realContent = decodedContent.replaceAll(mark, correctName);
+       zip.folder(realFolder).file(realFilename, encoder.encode(realContent), {binary:true});
+       count++;
+       if (count == urls.length) {
+         log("Generating ZIP file");
+         zip.generateAsync({type:'blob'}).then(function(content) {
+            log("ZIP created, saving...");
+            saveAs(content, correctName + ".zip");
+            log("Generation done for project " + correctName);
+            log("DONE!");
+         });
+      }
+    });
   });
-});
+}
