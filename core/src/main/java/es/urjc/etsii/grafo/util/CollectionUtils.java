b23fdb3a4831656c86b13ef6d758521e79e15bbb
Raúl Martín <raul.martin@urjc.es>
2021-06-15 12:29:38 +0200

    CollectionUtils tests, fixed bug

--- core/src/main/java/es/urjc/etsii/grafo/util/CollectionUtils.java
+++ core/src/main/java/es/urjc/etsii/grafo/util/CollectionUtils.java
@@ -19,7 +19,7 @@ public class CollectionUtils {
     public static <T> void reverseFragment(List<T> list, int start, int end) {
         assert list instanceof RandomAccess: "Reversing a fragment of a list where access is not O(1) is extremely slow. Think of a better way to do it ;)";
         assert start >= 0 && start <= list.size(): String.format("Start index (%s) must be in range [0, %s)", start, list.size());
-        assert end >= 0 && end <= list.size(): String.format("End index (%s) must be in range [0, %s)", start, list.size());
+        assert end >= 0 && end < list.size(): String.format("End index (%s) must be in range [0, %s)", start, list.size());
         assert start <= end: String.format("Start index (%s) must be <= end (%s)", start, end);
         for (int i = start, j = end; i < j; i++, j--) {
             T temp = list.get(i);
@@ -33,7 +33,7 @@ public class CollectionUtils {
      * @param list list to reverse
      */
     public static void reverse(List<Integer> list) {
-        reverseFragment(list, 0, list.size());
+        reverseFragment(list, 0, list.size() -1);
     }
 
     /**
@@ -59,7 +59,7 @@ public class CollectionUtils {
      * @param <T> List type
      * @return Chosen element
      */
-    public static <T> T pickRandom(ArrayList<T> list){
+    public static <T> T pickRandom(List<T> list){
         var random = RandomManager.getRandom();
         return list.get(random.nextInt(list.size()));
     }
---
+++ core/src/test/java/es/urjc/etsii/grafo/util/CollectionUtilsTests.java
@@ -0,0 +1,60 @@
+package es.urjc.etsii.grafo.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class CollectionUtilsTests {
+
+    @Test
+    public void reverseFragmentTest(){
+        var list = Arrays.asList(0,1,2,3,4,5);
+        CollectionUtils.reverseFragment(list, 0, 3);
+        Assertions.assertEquals(list, Arrays.asList(3,2,1,0,4,5));
+        CollectionUtils.reverseFragment(list, 0, 3);
+        Assertions.assertEquals(list, Arrays.asList(0,1,2,3,4,5));
+
+        CollectionUtils.reverseFragment(list, 3, list.size()-1);
+        Assertions.assertEquals(list, Arrays.asList(0,1,2,5,4,3));
+
+        // index is inclusive, bounds checks
+        Assertions.assertThrows(AssertionError.class, () -> CollectionUtils.reverseFragment(list, 0, list.size()));
+        Assertions.assertThrows(AssertionError.class, () -> CollectionUtils.reverseFragment(list, 3, 2));
+        Assertions.assertThrows(AssertionError.class, () -> CollectionUtils.reverseFragment(list, -1, 2));
+
+        // LinkedList for O(1) access should trigger assertion error
+        Assertions.assertThrows(AssertionError.class, () -> CollectionUtils.reverseFragment(new LinkedList<>(list), 0, 5));
+    }
+
+    @Test
+    public void reverseTest(){
+        var list = Arrays.asList(0,1,2,3,4,5);
+        CollectionUtils.reverse(list);
+        Assertions.assertEquals(list, Arrays.asList(5,4,3,2,1,0));
+    }
+
+    @Test
+    public void pickRandomFromSetTest(){
+        var initializeRandom = new RandomManager(0);
+        var set = new HashSet<>(Arrays.asList(0, 1, 2, 3, 4, 5));
+        for (int i = 0; i < 100; i++) {
+            int n = CollectionUtils.pickRandom(set);
+            Assertions.assertTrue(set.contains(n), String.format("Chosen element %s not in set %s", n, set));
+        }
+    }
+
+    @Test
+    public void pickRandomFromListTest(){
+        var initializeRandom = new RandomManager(0);
+        var list = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5));
+        for (int i = 0; i < 100; i++) {
+            int n = CollectionUtils.pickRandom(list);
+            Assertions.assertTrue(list.contains(n), String.format("Chosen element %s not in list %s", n, list));
+        }
+        //Assertions.assertThrows(AssertionError.class, () -> CollectionUtils.pickRandom(new LinkedList<>(list)));
+    }
+}
