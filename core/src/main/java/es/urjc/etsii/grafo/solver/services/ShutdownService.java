1d3bb132fdd991602793c47665e62cae17aa10cf
Raúl Martín <raul.martin@urjc.es>
2021-09-06 13:37:42 +0200

    Configurable stopOnExecutionEnd, defaults to true.

--- core/src/main/java/es/urjc/etsii/grafo/solver/services/ShutdownService.java
+++ core/src/main/java/es/urjc/etsii/grafo/solver/services/ShutdownService.java
@@ -1,12 +1,14 @@
 package es.urjc.etsii.grafo.solver.services;
 
 import es.urjc.etsii.grafo.solver.services.events.EventAsyncConfigurer;
+import es.urjc.etsii.grafo.solver.services.events.MorkEventListener;
 import es.urjc.etsii.grafo.solver.services.events.types.ExecutionEndedEvent;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 @Service
@@ -15,28 +17,36 @@ public class ShutdownService {
 
     private final ConfigurableApplicationContext appContext;
     private final EventAsyncConfigurer eventAsyncConfigurer;
+    private final boolean stopOnExperimentEnd;
 
-    public ShutdownService(ApplicationContext appContext, EventAsyncConfigurer eventAsyncConfigurer) {
+    public ShutdownService(ApplicationContext appContext, EventAsyncConfigurer eventAsyncConfigurer, @Value("${event.webserver.stopOnExecutionEnd:true}") boolean stopOnExperimentEnd) {
         this.appContext = (ConfigurableApplicationContext) appContext;
         this.eventAsyncConfigurer = eventAsyncConfigurer;
+        this.stopOnExperimentEnd = stopOnExperimentEnd;
     }
 
-    @EventListener
+    @MorkEventListener
     public void onExperimentationEnd(ExecutionEndedEvent event){
+        if(stopOnExperimentEnd){
+            log.info("StopOnExperimentEnd enabled, delayed stop executed.");
+            delayedStop(5, TimeUnit.SECONDS);
+        } else {
+            log.info("StopOnExperimentEnd disabled, app must be stopped by user");
+        }
+    }
+
+    public void delayedStop(long time, TimeUnit unit){
         new Thread(() ->{
             try {
-                Thread.sleep(5000);
+                Thread.sleep(unit.toMillis(time));
                 log.info("Shutting down Async Executor");
                 eventAsyncConfigurer.shutdownAsyncExecutor();
                 log.info("Closing context and exiting");
                 appContext.close();
-
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 log.warning(e.toString());
             }
         }).start();
-
     }
-
 }
