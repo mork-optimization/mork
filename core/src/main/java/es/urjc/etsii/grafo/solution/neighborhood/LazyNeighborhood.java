8950056f9c837238086b36f276b8758b42d650c5
Sergio Cavero <scaverod@gmail.com>
2021-11-15 17:41:49 +0100

    javadoc added

--- core/src/main/java/es/urjc/etsii/grafo/solution/neighborhood/LazyNeighborhood.java
+++ core/src/main/java/es/urjc/etsii/grafo/solution/neighborhood/LazyNeighborhood.java
@@ -22,6 +22,11 @@ public abstract class LazyNeighborhood<M extends LazyMove<S,I>, S extends Soluti
      */
     public abstract Stream<M> stream(S s);
 
+    /**
+     * Build an exhaustive stream that allows iterating the whole neighborhood given an initial move
+     * @param move initial move
+     * @return Stream with all the available moves in the neighborhood
+     */
     protected Stream<M> buildStream(M move){
         return Stream.iterate(move, Objects::nonNull, m -> (M) m.next());
     }
--- core/src/main/java/es/urjc/etsii/grafo/solver/services/ExperimentManager.java
+++ core/src/main/java/es/urjc/etsii/grafo/solver/services/ExperimentManager.java
@@ -12,14 +12,23 @@ import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
 @Service
-public class ExperimentManager<S extends Solution<S,I>, I extends Instance> {
+public class ExperimentManager<S extends Solution<S, I>, I extends Instance> {
 
     private Pattern experimentFilter;
 
     private static final Logger log = Logger.getLogger(Orquestrator.class.toString());
 
-    private final Map<String, List<Algorithm<S,I>>> experiments = new LinkedHashMap<>();
+    /**
+     * List of experiments
+     */
+    private final Map<String, List<Algorithm<S, I>>> experiments = new LinkedHashMap<>();
 
+    /**
+     * Constructor
+     * @param experimentImplementations list of experiments
+     * @param solverConfig solver configuration
+     * @param solutionBuilder solution builder
+     */
     public ExperimentManager(List<AbstractExperiment<S, I>> experimentImplementations, SolverConfig solverConfig, SolutionBuilder<S, I> solutionBuilder) {
         var experimentPattern = solverConfig.getExperiments();
         experimentFilter = Pattern.compile(experimentPattern);
@@ -30,7 +39,7 @@ public class ExperimentManager<S extends Solution<S,I>, I extends Instance> {
             validateAlgorithmNames(experiment.getName(), algorithms);
             String experimentName = experiment.getName();
             var matcher = experimentFilter.matcher(experimentName);
-            if(matcher.matches()){
+            if (matcher.matches()) {
                 this.experiments.put(experimentName, algorithms);
                 log.fine(String.format("Experiment %s matches against %s", experimentName, experimentPattern));
             } else {
@@ -39,31 +48,47 @@ public class ExperimentManager<S extends Solution<S,I>, I extends Instance> {
         }
     }
 
-    private void fillSolutionBuilder(Iterable<Algorithm<S,I>> algorithms, SolutionBuilder<S,I> solutionBuilder){
-        // For each algorithm in each experiment, set the solution builder reference.
-        for(var algorithm: algorithms){
+    /**
+     * For each algorithm in each experiment, set the solution builder reference.
+     *
+     * @param algorithms      list of algorithms
+     * @param solutionBuilder solution builder
+     */
+    private void fillSolutionBuilder(Iterable<Algorithm<S, I>> algorithms, SolutionBuilder<S, I> solutionBuilder) {
+        for (var algorithm : algorithms) {
             algorithm.setBuilder(solutionBuilder);
         }
     }
 
-    public Map<String, List<Algorithm<S, I>>> getExperiments(){
+    /**
+     * Returns a map with the list of algorithms for each of the experiments.
+     *
+     * @return mapping of experiments and it associated list of algorithms
+     */
+    public Map<String, List<Algorithm<S, I>>> getExperiments() {
         return Collections.unmodifiableMap(this.experiments);
     }
 
-    private void validateAlgorithmNames(String experimentName, List<Algorithm<S,I>> algorithms){
+    /**
+     * This procedure validates that there are no more than one algorithm with the same name (both toString and shortName)
+     *
+     * @param experimentName experiment name
+     * @param algorithms     list of algorithms
+     */
+    private void validateAlgorithmNames(String experimentName, List<Algorithm<S, I>> algorithms) {
         Set<String> toStrings = new HashSet<>();
         Set<String> shortNames = new HashSet<>();
-        for(var algorithm: algorithms){
+        for (var algorithm : algorithms) {
             // Check Algorithm::toString
             var toString = algorithm.toString();
-            if(toStrings.contains(toString)){
+            if (toStrings.contains(toString)) {
                 throw new IllegalArgumentException(String.format("Duplicated algorithm toString in experiment %s. FIX: All algorithm toString() should be unique per experiment → %s", experimentName, toString));
             }
             toStrings.add(toString);
 
             // Same check for Algorithm::getShortName
             var shortName = algorithm.getShortName();
-            if(shortNames.contains(shortName)){
+            if (shortNames.contains(shortName)) {
                 throw new IllegalArgumentException(String.format("Duplicated algorithm shortName in experiment %s. FIX: All algorithm getShortName() should be unique per experiment → %s", experimentName, shortName));
             }
             shortNames.add(shortName);
