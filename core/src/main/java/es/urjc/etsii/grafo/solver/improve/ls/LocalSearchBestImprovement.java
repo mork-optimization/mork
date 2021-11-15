a7ed1463a0fc38bc41d4198ad19578d6cecfee1d
Sergio Cavero <scaverod@gmail.com>
2021-11-15 11:17:22 +0100

    javadoc added

--- core/src/main/java/es/urjc/etsii/grafo/solver/improve/ls/LocalSearchBestImprovement.java
+++ core/src/main/java/es/urjc/etsii/grafo/solver/improve/ls/LocalSearchBestImprovement.java
@@ -9,23 +9,57 @@ import es.urjc.etsii.grafo.solution.neighborhood.Neighborhood;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+/**
+ * Local search procedures start from a given feasible solution and explore a determined neighborhood
+ * in each iteration, replacing the current solution if a neighbor solution improves the objective
+ * function of the current one. In this case, this local search procedure follows a Best Improvement strategy.
+ *  The best improvement strategy is related to executing the best move of the given neighbors.
+ *
+ * @param <M> the type of move
+ * @param <S> the type of problem solution
+ * @param <I> the type of problem instances
+ */
 public class LocalSearchBestImprovement<M extends Move<S, I>, S extends Solution<S,I>, I extends Instance> extends LocalSearch<M, S, I> {
 
+    /**
+     * Build a new local search
+     * @param comparator comparator to determine between two solutions which one is better
+     * @param ps neighborhood that generates the movements
+     */
     @SafeVarargs
     public LocalSearchBestImprovement(MoveComparator<M, S, I> comparator, Neighborhood<M, S, I>... ps) {
         super(comparator, ps);
     }
 
+    /**
+     * Build a new local search
+     *
+     * @param maximizing true if a movement with a bigger score is better
+     * @param ps         neighborhood that generates the movements
+     */
     @SafeVarargs
     public LocalSearchBestImprovement(boolean maximizing, Neighborhood<M, S, I>... ps) {
         super(maximizing, ps);
     }
 
+    /**
+     * Build a new local search
+     *
+     * @param lsName     Local Search name. If present, toString works as name{}. If not, Classname{neigh=[neigborhoods],comp=comparator}
+     * @param maximizing true if a movement with a bigger score is better
+     * @param ps         neighborhood that generates the movements
+     */
     @SafeVarargs
     public LocalSearchBestImprovement(boolean maximizing, String lsName, Neighborhood<M, S, I>... ps) {
         super(maximizing, lsName, ps);
     }
 
+    /**
+     * Get next move to execute.
+     *
+     * @param s Solution
+     * @return Proposed move
+     */
     @Override
     public Optional<M> getMove(S s) {
         M move = null;
@@ -41,6 +75,12 @@ public class LocalSearchBestImprovement<M extends Move<S, I>, S extends Solution
         return move != null && move.improves() ? Optional.of(move) : Optional.empty();
     }
 
+    /**
+     * Get best move to execute.
+     *
+     * @param stream a sequence of possible moves
+     * @return the best move of the stream
+     */
     private Optional<M> getBest(Stream<M> stream) {
         return stream.filter(Move::isValid).reduce((a, b) -> comparator.getBest(b, a));
     }
--- core/src/main/java/es/urjc/etsii/grafo/solver/improve/ls/LocalSearchFirstImprovement.java
+++ core/src/main/java/es/urjc/etsii/grafo/solver/improve/ls/LocalSearchFirstImprovement.java
@@ -8,24 +8,58 @@ import es.urjc.etsii.grafo.solution.neighborhood.Neighborhood;
 
 import java.util.Optional;
 
+/**
+ * Local search procedures start from a given feasible solution and explore a determined neighborhood
+ * in each iteration, replacing the current solution if a neighbor solution improves the objective
+ * function of the current one. In this case, this local search procedure follows a First Improvement strategy.
+ *  The first improvement strategy is related to executing the first move that improves the current solution
+ *
+ * @param <M> the type of move
+ * @param <S> the type of problem solution
+ * @param <I> the type of problem instances
+ */
 public class LocalSearchFirstImprovement<M extends Move<S, I>, S extends Solution<S,I>, I extends Instance> extends LocalSearch<M, S, I> {
 
+    /**
+     * Build a new local search
+     * @param comparator comparator to determine between two solutions which one is better
+     * @param ps neighborhood that generates the movements
+     */
     @SafeVarargs
     public LocalSearchFirstImprovement(MoveComparator<M, S, I> comparator, Neighborhood<M, S, I>... ps) {
         super(comparator, ps);
     }
 
+    /**
+     * Build a new local search
+     *
+     * @param maximizing true if a movement with a bigger score is better
+     * @param ps         neighborhood that generates the movements
+     */
     @SafeVarargs
     public LocalSearchFirstImprovement(boolean maximizing, Neighborhood<M, S, I>... ps) {
         super(maximizing, ps);
     }
 
 
+    /**
+     * Build a new local search
+     *
+     * @param lsName     Local Search name. If present, toString works as name{}. If not, Classname{neigh=[neigborhoods],comp=comparator}
+     * @param maximizing true if a movement with a bigger score is better
+     * @param ps         neighborhood that generates the movements
+     */
     @SafeVarargs
     public LocalSearchFirstImprovement(boolean maximizing, String lsName, Neighborhood<M, S, I>... ps) {
         super(maximizing, lsName, ps);
     }
 
+    /**
+     * Get next move to execute.
+     *
+     * @param s Solution
+     * @return Proposed move
+     */
     @Override
     public Optional<M> getMove(S s) {
         M move = null;
