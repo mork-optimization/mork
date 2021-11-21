96fc92747b16afbff339ba4355ec2d3de2ccc076
Sergio Cavero <scaverod@gmail.com>
2021-11-22 00:31:28 +0100

    TSP instance implemented

--- examples/TSP/src/main/java/es/urjc/etsii/grafo/TSP/model/TSPInstance.java
+++ examples/TSP/src/main/java/es/urjc/etsii/grafo/TSP/model/TSPInstance.java
@@ -4,11 +4,44 @@ import es.urjc.etsii.grafo.io.Instance;
 
 public class TSPInstance extends Instance {
 
-    public TSPInstance(String name){
+    /**
+     * List of coordinates
+     */
+    private final Coordinate[] locations;
+
+
+    /**
+     * Constructor
+     *
+     * @param name      name of the instance
+     * @param locations list of coordiantes
+     */
+    protected TSPInstance(String name, Coordinate[] locations) {
         super(name);
-        // TODO Add all required fields and parameters
+        this.locations = locations;
     }
 
+    /**
+     * Get the list of locations
+     *
+     * @return list of locations
+     */
+    public Coordinate[] getLocations() {
+        return locations;
+    }
 
-    // compareTo may be overriden to specify a custom instance solving order (default ordering by instance file name)
+    /**
+     * Get the number of locations of the instance
+     *
+     * @return number of locations
+     */
+    public int numberOfLocations() {
+        return locations.length;
+    }
+
+    /**
+     * 2D coordinate
+     */
+    public record Coordinate(double x, double y) {
+    }
 }
