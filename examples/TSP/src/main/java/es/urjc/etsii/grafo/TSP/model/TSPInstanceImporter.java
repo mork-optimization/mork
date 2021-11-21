799ac0307cf5bc90eb439a7cddffd205030aef3c
Sergio Cavero <scaverod@gmail.com>
2021-11-22 00:32:11 +0100

    TSP instance importer implemented

--- examples/TSP/src/main/java/es/urjc/etsii/grafo/TSP/model/TSPInstanceImporter.java
+++ examples/TSP/src/main/java/es/urjc/etsii/grafo/TSP/model/TSPInstanceImporter.java
@@ -4,19 +4,28 @@ import es.urjc.etsii.grafo.io.InstanceImporter;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class TSPInstanceImporter extends InstanceImporter<TSPInstance> {
 
     @Override
     public TSPInstance importInstance(BufferedReader reader, String filename) throws IOException {
-        // Create and return instance object from file data
-        // TODO parse all data from the given reader however I want
-        // TIP You may use a Scanner if you prefer it to a Buffered Reader:
-        // Scanner sc = new Scanner(reader);
-
-        var instance = new TSPInstance(filename);
-
-        // IMPORTANT! Remember that instance data must be immutable from this point
-        return instance;
+        Scanner sc = new Scanner(reader).useLocale(Locale.US);
+        String name = sc.nextLine().trim().split(":")[1];
+        String type = sc.nextLine().split(":")[1];
+        String comment = sc.nextLine().split(":")[1];
+        int dimension = Integer.parseInt(sc.nextLine().split(":")[1].trim());
+        String edgeWeightType = sc.nextLine().split(":")[1];
+        String displayDataType = sc.nextLine();
+        String nodeCoordSection = sc.nextLine();
+        TSPInstance.Coordinate[] locations = new TSPInstance.Coordinate[dimension];
+        while (!sc.hasNext("EOF")) {
+            int id = sc.nextInt() - 1;
+            double x = sc.nextDouble();
+            double y = sc.nextDouble();
+            locations[id] = new TSPInstance.Coordinate(x, y);
+        }
+        return new TSPInstance(name, locations);
     }
 }
