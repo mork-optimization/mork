489bc3a5999653f99fe3a9310e3c99e5a97b40e8
Raúl Martín <raul.martin@urjc.es>
2021-12-20 16:13:32 +0100

    better SmokeTest

--- core/src/test/java/es/urjc/etsii/grafo/solver/SmokeTest.java
+++ core/src/test/java/es/urjc/etsii/grafo/solver/SmokeTest.java
@@ -1,13 +1,14 @@
-package es.urjc.etsii.grafo.TSP;
+package es.urjc.etsii.grafo.solver;
 
-import es.urjc.etsii.grafo.solver.Mork;
 import es.urjc.etsii.grafo.solver.executors.Executor;
 import es.urjc.etsii.grafo.solver.services.AbstractOrchestrator;
 import es.urjc.etsii.grafo.solver.services.events.AbstractEventStorage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = {Mork.class})
@@ -23,11 +24,22 @@ class SmokeTest {
     @Autowired
     private AbstractEventStorage abstractEventStorage;
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     public void checkInjected(){
         // There must exactly one implementation of the following components loaded
         Assertions.assertNotNull(orchestrator);
         Assertions.assertNotNull(executor);
         Assertions.assertNotNull(abstractEventStorage);
+        Assertions.assertNotNull(applicationContext);
+    }
+
+    @Test
+    public void checkNotRunner(){
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class,
+                () -> this.applicationContext.getBean(RunOnStart.class),
+                "CommandLineRunner should not be loaded during this integration test");
     }
 }
