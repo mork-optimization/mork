52f0a7e9a1592cccba394e1f90138add83d261a1
Raúl Martín <raul.martin@urjc.es>
2022-07-19 16:52:04 +0200

    Optimization: Cache instance solve order

--- core/src/main/java/es/urjc/etsii/grafo/io/InstanceManager.java
+++ core/src/main/java/es/urjc/etsii/grafo/io/InstanceManager.java
@@ -27,6 +27,7 @@ public class InstanceManager<I extends Instance> {
     private final InstanceImporter<I> instanceImporter;
 
     private final Map<String, SoftReference<I>> cacheByPath;
+    private final Map<String, List<String>> solveOrderByExperiment;
 
 
     /**
@@ -38,6 +39,7 @@ public class InstanceManager<I extends Instance> {
         this.instanceConfiguration = instanceConfiguration;
         this.instanceImporter = instanceImporter;
         this.cacheByPath = new ConcurrentHashMap<>();
+        this.solveOrderByExperiment = new ConcurrentHashMap<>();
     }
 
 
@@ -47,19 +49,20 @@ public class InstanceManager<I extends Instance> {
      * @param expName experiment name as string
      * @return Ordered list of instance identifiers, that can be later used by the getInstance method. Instances should be solved in the returned order.
      */
-    public List<String> getInstanceSolveOrder(String expName){
-        String instancePath = this.instanceConfiguration.getPath(expName);
-        checkExists(instancePath);
-        List<Path> instances = IOUtil.iterate(instancePath);
+    public synchronized List<String> getInstanceSolveOrder(String expName){
+        return this.solveOrderByExperiment.computeIfAbsent(expName, s -> {
+            String instancePath = this.instanceConfiguration.getPath(expName);
+            checkExists(instancePath);
+            List<Path> instances = IOUtil.iterate(instancePath);
 
-        List<String> sortedInstances;
-        if(this.instanceConfiguration.isPreload()){
-            sortedInstances = validateAndSort(expName, instances);
-        } else {
-            sortedInstances = lexicSort(instances);
-        }
-
-        return sortedInstances;
+            List<String> sortedInstances;
+            if(this.instanceConfiguration.isPreload()){
+                sortedInstances = validateAndSort(expName, instances);
+            } else {
+                sortedInstances = lexicSort(instances);
+            }
+            return sortedInstances;
+        });
     }
 
     protected List<String> validateAndSort(String expName, List<Path> instancePaths) {
