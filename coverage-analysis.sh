7f867a236f7afeb06a44d24d63032775b91fffe3
Raúl Martín <raul.martin@urjc.es>
2022-08-25 16:19:05 +0200

    coverage-analysis.sh: clean project before run and open results after running

--- coverage-analysis.sh
+++ coverage-analysis.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
-mvn -Dgpg.skip -B jacoco:prepare-agent verify
+mvn -Dgpg.skip -B clean jacoco:prepare-agent verify
+
+path="example-tsp/target/site/jacoco-aggregate/index.html"
+if command -v open > /dev/null 2>&1
+then
+    open "$path"
+    exit
+fi
+if command -v xdg-open > /dev/null 2>&1
+then
+    xdg-open "$path"
+    exit
+else
+    echo "Results available at '$path'"
+fi
