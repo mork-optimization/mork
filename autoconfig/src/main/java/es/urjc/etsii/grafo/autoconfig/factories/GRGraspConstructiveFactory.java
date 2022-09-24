b73298c73d624dc96fc5b5768c23ab97b76f6a0e
Raúl Martín <raul.martin@urjc.es>
2022-09-25 00:24:42 +0200

    GraspFactory test and refactor

--- autoconfig/src/main/java/es/urjc/etsii/grafo/autoconfig/factories/GRGraspConstructiveFactory.java
+++ autoconfig/src/main/java/es/urjc/etsii/grafo/autoconfig/factories/GRGraspConstructiveFactory.java
@@ -1,42 +1,16 @@
 package es.urjc.etsii.grafo.autoconfig.factories;
 
-import es.urjc.etsii.grafo.annotations.ProvidedParamType;
-import es.urjc.etsii.grafo.autoconfig.irace.params.ComponentParameter;
-import es.urjc.etsii.grafo.autoconfig.irace.params.ParameterType;
-import es.urjc.etsii.grafo.autoconfig.service.factories.AlgorithmComponentFactory;
-import es.urjc.etsii.grafo.create.grasp.GRASPListManager;
-import es.urjc.etsii.grafo.create.grasp.GraspBuilder;
 import es.urjc.etsii.grafo.create.grasp.GreedyRandomGRASPConstructive;
 
-import java.util.List;
 import java.util.Map;
 
-public class GRGraspConstructiveFactory extends AlgorithmComponentFactory {
+public class GRGraspConstructiveFactory extends GraspConstructiveFactory {
 
     @Override
     public Object buildComponent(Map<String, Object> params) {
-        var graspBuilder = new GraspBuilder();
-        graspBuilder.withMaximizing((boolean) params.get("maximize"));
-        if(params.containsKey("alphaMin") && params.containsKey("alphaMax")){
-            graspBuilder.withAlphaInRange((double) params.get("alphaMin"), (double) params.get("alphaMax"));
-        } else if(params.containsKey("alpha")){
-            graspBuilder.withAlphaValue((double) params.get("alpha"));
-        } else {
-            graspBuilder.withAlphaRandom();
-        }
-        // TODO review graspBuilder.withGreedyFunction();
-        graspBuilder.withListManager((GRASPListManager) params.get("candidateListManager"));
+        var graspBuilder = super.initBuilder(params);
         graspBuilder.withStrategyGreedyRandom();
         return graspBuilder.build();
-    }
-
-    @Override
-    public List<ComponentParameter> getRequiredParameters() {
-        return List.of(
-                new ComponentParameter("alpha", Double.TYPE, ParameterType.REAL, 0, 1),
-                new ComponentParameter("maximize", Boolean.TYPE, ParameterType.PROVIDED, new Object[]{ProvidedParamType.MAXIMIZE}),
-                new ComponentParameter("candidateListManager", GRASPListManager.class, ParameterType.NOT_ANNOTATED, new Object[]{})
-        );
     }
 
     @Override
