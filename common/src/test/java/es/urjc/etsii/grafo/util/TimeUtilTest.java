642dd80033977124d891496d222496ac00da94da
Raúl Martín <raul.martin@urjc.es>
2022-09-26 15:08:17 +0200

    Improve TimeUtilTest

--- common/src/test/java/es/urjc/etsii/grafo/util/TimeUtilTest.java
+++ common/src/test/java/es/urjc/etsii/grafo/util/TimeUtilTest.java
@@ -3,21 +3,31 @@ package es.urjc.etsii.grafo.util;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class TimeUtilTest {
+import java.util.concurrent.TimeUnit;
+
+class TimeUtilTest {
 
     @Test
-    public void testNanosToSecs(){
+    void testNanosToSecs(){
         Assertions.assertEquals(1, TimeUtil.nanosToSecs(1_000_000_000));
         Assertions.assertEquals(-2, TimeUtil.nanosToSecs(-2_000_000_000));
         Assertions.assertEquals(0, TimeUtil.nanosToSecs(0));
     }
 
     @Test
-    public void testSecsToNanos(){
+    void testSecsToNanos(){
         Assertions.assertEquals(1_000_000_000, TimeUtil.secsToNanos(1));
         Assertions.assertEquals(-2_000_000_000, TimeUtil.secsToNanos(-2));
         Assertions.assertEquals(5_123_456_000L, TimeUtil.secsToNanos(5.123456));
         Assertions.assertEquals(0, TimeUtil.secsToNanos(0));
+    }
 
+    @Test
+    void convertTest(){
+        Assertions.assertEquals(24, TimeUtil.convert(1, TimeUnit.DAYS, TimeUnit.HOURS));
+        Assertions.assertEquals(36_000, TimeUtil.convert(10, TimeUnit.HOURS, TimeUnit.SECONDS));
+        Assertions.assertEquals(10_000, TimeUtil.convert(10, TimeUnit.SECONDS, TimeUnit.MILLISECONDS));
+        Assertions.assertEquals(TimeUtil.NANOS_IN_SECOND, TimeUtil.convert(1, TimeUnit.SECONDS, TimeUnit.NANOSECONDS));
+        Assertions.assertEquals(TimeUtil.NANOS_IN_MILLISECOND, TimeUtil.convert(1, TimeUnit.MILLISECONDS, TimeUnit.SECONDS));
     }
 }
