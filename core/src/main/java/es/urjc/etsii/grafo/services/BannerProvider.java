afecd30e74bee693fa0a4693b7926757cba0f26d
Raúl Martín <raul.martin@urjc.es>
2022-12-12 10:13:14 +0100

    Improve banner

--- core/src/main/java/es/urjc/etsii/grafo/services/BannerProvider.java
+++ core/src/main/java/es/urjc/etsii/grafo/services/BannerProvider.java
@@ -19,15 +19,16 @@ public class BannerProvider implements Banner {
     @Override
     public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
 
-        String banner = "   ____  __  __            _     ____\n" +
-                "  / / / |  \\/  |          | |    \\ \\ \\\n" +
-                " | | |  | \\  / | ___  _ __| | __  | | |\n" +
-                " | | |  | |\\/| |/ _ \\| '__| |/ /  | | |\n" +
-                " | | |  | |  | | (_) | |  |   <   | | |\n" +
-                " | | |  |_|  |_|\\___/|_|  |_|\\_\\  | | |\n" +
-                "  \\_\\_\\ ________________________ /_/_/\n" +
-                "                        Version: " + Mork.class.getPackage().getImplementationVersion() + "\n" +
-                "                        Launched by: " + sourceClass.getName();
+        String version = Mork.class.getPackage().getImplementationVersion();
+        String banner = """
+                ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+                █▄─▀█▀─▄█─▄▄─█▄─▄▄▀█▄─█─▄█
+                ██─█▄█─██─██─██─▄─▄██─▄▀██
+                █▄▄▄█▄▄▄█▄▄▄▄█▄▄█▄▄█▄▄█▄▄█
+                   Version: %s
+                   """
+                .indent(1)
+                .formatted(version);
 
         out.println(banner);
     }
