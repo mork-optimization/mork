a21f6b9e52cdf89bcf744abcbe4377797b477966
Raúl Martín <raul.martin@urjc.es>
2023-10-01 17:03:30 +0200

    Improve release.sh

--- release.sh
+++ release.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -e
 #mvn clean install
 
+# Check if gpg signing is correctly configured
+echo "" | gpg --sign >/dev/null
+
+# Do release
 mvn -B release:clean release:prepare release:perform
