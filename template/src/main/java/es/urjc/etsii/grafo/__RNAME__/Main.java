8f136fb1a90625567ef8cfbec8cd4aa1bea10d04
Raúl Martín <raul.martin@urjc.es>
2024-10-09 12:52:40 +0200

    Small main template improvement

--- template/src/main/java/es/urjc/etsii/grafo/__RNAME__/Main.java
+++ template/src/main/java/es/urjc/etsii/grafo/__RNAME__/Main.java
@@ -7,11 +7,17 @@ import es.urjc.etsii.grafo.solver.Mork;
 
 public class Main {
 
-    // Replace Objective1 with your objective name, possible examples are: "Distance", "Cost", etc.
     public static final String OBJECTIVE_NAME = "Objective1";
 
     public static void main(String[] args) {
         // TODO Configure your objectives
-        Mork.start(args, Objective.ofMinimizing(OBJECTIVE_NAME, __RNAME__Solution::getScore, __RNAME__BaseMove::getScoreChange));
+        // Replace Objective1 with your objective name, possible examples are: "Distance", "Cost", etc.
+        var myObjective = Objective.ofMinimizing("objectiveName", __RNAME__Solution::getScore, __RNAME__BaseMove::getScoreChange);
+        Mork.start(args, myObjective);
+
+        // For a more flexible approach, you can use the following method:
+        // Mork.start(args, multiobjective:true/false, myObjective1, myObjective2, myObjective3);
+        // See the docs for more information on how to configure your objectives if using alternative objective functions
+        // or you are solving a multi-objective problem
     }
 }
