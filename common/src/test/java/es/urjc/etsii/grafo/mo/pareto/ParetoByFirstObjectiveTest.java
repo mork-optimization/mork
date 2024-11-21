75a904b0fc300d31b6d02bb3ba34f3757c60ff48
Javier Yuste <javier.yuste@urjc.es>
2024-11-21 18:28:40 +0100

    Added unit tests for ParetoSet implementations (#270)
    * Changed type of parameter received in methods dedicated to generate lists of TestMove objects, since variable score is a double.
    
    * Added two classes that are useful to implement unit tests in multi-objective contexts. TestSolutionWithMultipleObjectives is similar to TestSolution, but can have more than one objective. TestMoveWithMultipleObjectives is similar to TestMove, but can have more than one objective.
    
    * Changed hashCode method to be consistent with equals.
    
    * Added unit tests for ParetoSimpleList class.
    
    * Removed unnecessary line.
    
    * Added unit tests for ParetoByFirstObjective and NDTree.
    
    * Added MOAlgorithm, needed to define a generic algorithm that returns a ParetoSet instead of a single solution.

---
+++ common/src/test/java/es/urjc/etsii/grafo/mo/pareto/ParetoByFirstObjectiveTest.java
@@ -0,0 +1,193 @@
+package es.urjc.etsii.grafo.mo.pareto;
+
+import es.urjc.etsii.grafo.algorithms.FMode;
+import es.urjc.etsii.grafo.solution.Objective;
+import es.urjc.etsii.grafo.testutil.TestInstance;
+import es.urjc.etsii.grafo.testutil.TestMoveWithMultipleObjectives;
+import es.urjc.etsii.grafo.testutil.TestSolutionWithMultipleObjectives;
+import es.urjc.etsii.grafo.util.Context;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ParetoByFirstObjectiveTest {
+
+    static ParetoSet<TestSolutionWithMultipleObjectives, TestInstance> paretoSet;
+
+    public static void initializeParetoSet(){
+        paretoSet = new ParetoByFirstObjective<>();
+    }
+
+    public static void setNObjectives(int n){
+        Objective<TestMoveWithMultipleObjectives,
+                TestSolutionWithMultipleObjectives,
+                TestInstance>[] objectives = new Objective[n];
+
+        for (int i = 0; i < n; i++) {
+            int finalI = i;
+            objectives[i] = Objective.of(
+                    "obj" + i,
+                    FMode.MINIMIZE,
+                    s -> s.getObjective(finalI),
+                    m -> m.getScoreChanges()[finalI]
+            );
+        }
+
+        Context.Configurator.setObjectives(true, objectives);
+    }
+
+    public static void setMaxTrackedSolutions(int max){
+        Context.Pareto.MAX_TRACKED_SOLS = max;
+    }
+
+    @Test
+    public void testAddingDominatingSolution(){
+        setNObjectives(2);
+        initializeParetoSet();
+
+        TestInstance instance = new TestInstance("testInstance");
+
+        Assertions.assertTrue(
+                paretoSet.add(
+                        new TestSolutionWithMultipleObjectives(
+                                instance,
+                                new double[]{0.0, 0.0}
+                        )
+                )
+        );
+
+        Assertions.assertTrue(
+                paretoSet.add(
+                        new TestSolutionWithMultipleObjectives(
+                                instance,
+                                new double[]{-1.0, -1.0}
+                        )
+                )
+        );
+
+        Assertions.assertEquals(1, paretoSet.size());
+    }
+
+    @Test
+    public void testAddingDominatedSolution(){
+        setNObjectives(2);
+        initializeParetoSet();
+
+        TestInstance instance = new TestInstance("testInstance");
+
+        Assertions.assertTrue(
+                paretoSet.add(
+                        new TestSolutionWithMultipleObjectives(
+                                instance,
+                                new double[]{0.0, 0.0}
+                        )
+                )
+        );
+
+        Assertions.assertFalse(
+                paretoSet.add(
+                        new TestSolutionWithMultipleObjectives(
+                                instance,
+                                new double[]{1.0, 0.0}
+                        )
+                )
+        );
+
+        Assertions.assertEquals(1, paretoSet.size());
+    }
+
+    @Test
+    public void testAddingNonDominatedSolutions(){
+        setNObjectives(2);
+        initializeParetoSet();
+
+        TestInstance instance = new TestInstance("testInstance");
+        int additions = 20;
+        for (int i = 0; i < additions; i++) {
+            Assertions.assertTrue(
+                    paretoSet.add(
+                            new TestSolutionWithMultipleObjectives(
+                                    instance,
+                                    new double[]{0.0 - i, 0.0 + i}
+                            )
+                    )
+            );
+        }
+
+        Assertions.assertEquals(additions, paretoSet.size());
+
+        Assertions.assertTrue(
+                paretoSet.add(
+                        new TestSolutionWithMultipleObjectives(
+                                instance,
+                                new double[]{0.0 - additions, 0.0 - additions}
+                        )
+                )
+        );
+
+        Assertions.assertEquals(1, paretoSet.size());
+    }
+
+    @Test
+    public void testGetTrackedSolutions(){
+        setNObjectives(2);
+        initializeParetoSet();
+        TestInstance instance = new TestInstance("testInstance");
+        int additions = 20;
+        setMaxTrackedSolutions(additions);
+        Set<TestSolutionWithMultipleObjectives> solutions = HashSet.newHashSet(additions);
+
+        for (int i = 0; i < additions; i++) {
+            TestSolutionWithMultipleObjectives sol = new TestSolutionWithMultipleObjectives(
+                    instance,
+                    new double[]{0.0 - i, 0.0 + i}
+            );
+            solutions.add(sol);
+            Assertions.assertTrue(
+                    paretoSet.add(
+                            sol
+                    )
+            );
+        }
+
+        for (TestSolutionWithMultipleObjectives trackedSolution : paretoSet.getTrackedSolutions()) {
+            Assertions.assertTrue(solutions.contains(trackedSolution));
+            solutions.remove(trackedSolution);
+        }
+
+        Assertions.assertTrue(solutions.isEmpty());
+
+        TestSolutionWithMultipleObjectives sol = new TestSolutionWithMultipleObjectives(
+                instance,
+                new double[]{0.0 - additions, 0.0 + additions}
+        );
+        Assertions.assertTrue(
+                paretoSet.add(
+                        sol
+                )
+        );
+
+        for (TestSolutionWithMultipleObjectives trackedSolution : paretoSet.getTrackedSolutions()) {
+            Assertions.assertNotEquals(sol, trackedSolution);
+        }
+
+        Assertions.assertEquals(additions + 1, paretoSet.size());
+
+        sol = new TestSolutionWithMultipleObjectives(
+                instance,
+                new double[]{Integer.MIN_VALUE, Integer.MIN_VALUE}
+        );
+        Assertions.assertTrue(
+                paretoSet.add(
+                        sol
+                )
+        );
+        Assertions.assertEquals(1, paretoSet.size());
+        for (TestSolutionWithMultipleObjectives trackedSolution : paretoSet.getTrackedSolutions()) {
+            Assertions.assertEquals(sol, trackedSolution);
+        }
+    }
+
+}
