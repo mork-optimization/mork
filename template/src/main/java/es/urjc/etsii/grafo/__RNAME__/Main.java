9927b6c8ad7dae64473072b95ac08409e5e29f0c
Raúl Martín <raul.martin@urjc.es>
2025-04-09 14:44:06 +0200

    Update template, ref results available by default

--- template/src/main/java/es/urjc/etsii/grafo/__RNAME__/Main.java
+++ template/src/main/java/es/urjc/etsii/grafo/__RNAME__/Main.java
@@ -1,19 +1,21 @@
 package es.urjc.etsii.grafo.__RNAME__;
 
 import es.urjc.etsii.grafo.__RNAME__.model.__RNAME__BaseMove;
+import es.urjc.etsii.grafo.__RNAME__.model.__RNAME__Instance;
 import es.urjc.etsii.grafo.__RNAME__.model.__RNAME__Solution;
 import es.urjc.etsii.grafo.solution.Objective;
 import es.urjc.etsii.grafo.solver.Mork;
 
 public class Main {
 
-    public static final String OBJECTIVE_NAME = "Objective1";
+    // TODO Configure your objective
+    // Replace Objective1 with your objective name, possible examples are: "Distance", "Cost", etc.
+    // If maximizing, replace ofMinimizing with ofMaximizing
+    public static final Objective<__RNAME__BaseMove, __RNAME__Solution, __RNAME__Instance> OBJECTIVE =
+            Objective.ofMinimizing("Objective1", __RNAME__Solution::getScore, __RNAME__BaseMove::getScoreChange);
 
     public static void main(String[] args) {
-        // TODO Configure your objectives
-        // Replace Objective1 with your objective name, possible examples are: "Distance", "Cost", etc.
-        var myObjective = Objective.ofMinimizing("objectiveName", __RNAME__Solution::getScore, __RNAME__BaseMove::getScoreChange);
-        Mork.start(args, myObjective);
+        Mork.start(args, OBJECTIVE);
 
         // For a more flexible approach, you can use the following method:
         // Mork.start(args, multiobjective:true/false, myObjective1, myObjective2, myObjective3);
