7d0b9eb154505eaeea2e7b512609be2b7c6b44ac
Raúl Martín <raul.martin@urjc.es>
2025-04-10 16:47:40 +0200

    Fix tests: ImproverTest bug and forkcount

--- common/src/test/java/es/urjc/etsii/grafo/improve/ImproverTest.java
+++ common/src/test/java/es/urjc/etsii/grafo/improve/ImproverTest.java
@@ -1,16 +1,24 @@
 package es.urjc.etsii.grafo.improve;
 
+import es.urjc.etsii.grafo.algorithms.FMode;
 import es.urjc.etsii.grafo.metrics.Metrics;
+import es.urjc.etsii.grafo.solution.Objective;
 import es.urjc.etsii.grafo.testutil.TestInstance;
+import es.urjc.etsii.grafo.testutil.TestMove;
 import es.urjc.etsii.grafo.testutil.TestSolution;
+import es.urjc.etsii.grafo.util.Context;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class ImproverTest {
+
+    private static final Objective<TestMove, TestSolution, TestInstance> OBJ_MAX = Objective.of("Test", FMode.MAXIMIZE, TestSolution::getScore, TestMove::getScoreChange);
+
     @BeforeAll
     public static void init(){
         Metrics.disableMetrics();
+        Context.Configurator.setObjectives(OBJ_MAX);
     }
 
     @Test
